e5d9656306a46530ffe25980c96bac61faf2f75f
canerolu2019 <canerolu1@gmail.com>
2021-05-18 10:57:22 +0300

    exercitii

---
+++ Logical Operators/task5.js
@@ -0,0 +1,10 @@
+alert( null || 2 && 3 || 4 ); 
+
+// the answer is 3// 
+
+//The precedence of AND && is higher than ||, so it executes first.
+
+//The result of 2 && 3 = 3, so the expression becomes:
+
+null || 3 || 4
+//Now the result is the first truthy value: 3. //
